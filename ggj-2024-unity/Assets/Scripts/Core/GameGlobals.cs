444708ac1a320d47d7dcb9d3937dc25e8152a168
David Evans <david0evans@gmail.com>
2024-01-26 16:30:39 +0900

    refactor game globals to SO

--- ggj-2024-unity/Assets/Scripts/Core/GameGlobals.cs
+++ ggj-2024-unity/Assets/Scripts/Core/GameGlobals.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
-public class GameGlobals : Singleton<GameGlobals>
+[CreateAssetMenu(fileName = "GameGlobals", menuName = "Game/Game Globals")]
+public class GameGlobals : ScriptableObject
 {
+  public static GameGlobals Instance { get; private set; }
+
   public AnimationCurve UIHydrateCurve;
   public AnimationCurve UIDehydrateCurve;
 
-  private void Awake()
+  private void OnEnable()
   {
     Instance = this;
   }
