294e16d9a67b3a7d152d125bc2fd6092c4d87ddd
David Evans <david0evans@gmail.com>
2024-01-28 17:58:39 +0900

    crows toss their food a bit up, fix food pivots

--- ggj-2024-unity/Assets/Scripts/Character/CrowBehaviorManager.cs
+++ ggj-2024-unity/Assets/Scripts/Character/CrowBehaviorManager.cs
@@ -454,7 +454,7 @@ public class CrowBehaviorManager : MonoBehaviour
     }
 
     // Vomit them up
-    Vector3 vomitForce = transform.forward * VomitStrength;
+    Vector3 vomitForce = transform.forward.WithY(1) * VomitStrength;
     foreach (ItemDefinition itemDefinition in playerItems)
     {
       _inventoryController.TossItem(itemDefinition, vomitForce);
