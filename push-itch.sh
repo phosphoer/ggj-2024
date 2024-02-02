721859da33d4513c1e085a5053f21f52bb89f71e
David Evans <david0evans@gmail.com>
2024-02-01 16:19:05 -0800

    update font in interact prefab, create linux build script and add itch push script

---
+++ push-itch.sh
@@ -0,0 +1,3 @@
+~/Library/'Application Support'/itch/broth/butler/versions/15.21.0/butler push ggj-2024-unity/build/caws-mac stickybiscuits/lostcaws:osx
+~/Library/'Application Support'/itch/broth/butler/versions/15.21.0/butler push ggj-2024-unity/build/caws-windows stickybiscuits/lostcaws:windows
+~/Library/'Application Support'/itch/broth/butler/versions/15.21.0/butler push ggj-2024-unity/build/caws-linux stickybiscuits/lostcaws:linux
